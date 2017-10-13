0db5281dfd0be10edcd53d18f0aff4f0be5cf90d
Malay Mehta <meetmalay@gmail.com>
2017-10-13 11:46:03 +0530

    Changing the readme file contents and Database Seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,7 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->delete();
         DB::table('users')->insert(array(
-            array('name'=>'admin','email'=>'admin@oscar.co.uk', 'password'=>\Illuminate\Support\Facades\Hash::make('password'))
+            array('name'=>'admin','email'=>'contact@helloworld.com', 'password'=>\Illuminate\Support\Facades\Hash::make('happy2017'))
         ));
     }
 }
